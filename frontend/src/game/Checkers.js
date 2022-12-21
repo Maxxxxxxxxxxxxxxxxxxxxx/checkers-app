cd199f7093ee0ff84c7bed18187e327637e762c6
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2022-12-21 20:13:43 +0100

    MAJOR: move game logic to client-side

---
+++ frontend/src/game/Checkers.js
@@ -0,0 +1,82 @@
+import * as board from "./board.json" assert {type: 'json'};
+import { Vector } from "vector2d";
+
+const randomId = () => [...Array(5)].map(e => Math.floor(Math.random() * 9))
+const positionsTop = [
+    [1, 0],[3, 0],[5, 0],[7, 0],
+    [0, 1],[2, 1],[4, 1],[6, 1],
+    [1, 2],[3, 2],[5, 2],[7, 2] 
+]
+const positionsBottom = [
+    [0, 7],[2, 7],[4, 7],[6, 7],
+    [1, 6],[3, 6],[5, 6],[7, 6],
+    [0, 5],[2, 5],[4, 5],[6, 5]
+]
+const mapPawns = (side, positions) => positions.map((field, index) => {
+    return {
+        isQueen: false,
+        isDead: false,
+        side,
+        index,
+        pos: {
+            x: field[0],
+            y: field[1]
+        }
+    }
+});
+
+// let x = i32::abs(start.x - end.x);
+// let y = i32::abs(start.y - end.y);
+
+
+class GameState {
+    constructor(colorTop, colorBottom) {
+        this.player_black_id
+        this.player_white_id
+        this.current_color = "w"
+        this.turn = 1
+        this.id = randomId()
+        this.moves = []
+        this.pawns = [mapPawns(colorTop, positionsTop), mapPawns(colorBottom, positionsBottom)].flatMap(e => e)
+    }
+    // move = (side, index, x, y) => {
+    //     this.pawns = this.pawns.map(pawn => pawn.pos.x === destX && pawn.pos.y === destY ? { ...pawn, pos: { x: destX, y: destY } } : pawn);
+    // }
+    validateMove = (pawn, destX, destY) => {
+        let xShift = Math.abs(destX - pawn.pos.x);
+        let yShift = Math.abs(destY - pawn.pos.y);
+    
+        if (!pawn.isQueen) {
+            // normal pawn move case
+            if (xShift === 1 && yShift === 1) {
+                // check if destination is occupied already
+                if (this.pawns.find(pawn => pawn.pos.x === destX && pawn.pos.y === destY)) return false
+                else this.pawns = this.pawns.map(pawn => pawn.pos.x === destX && pawn.pos.y === destY ? { ...pawn, pos: { x: destX, y: destY } } : pawn);
+                return true
+            }
+            if (xShift === 2 && yShift === 2) {
+                // get the directional vector
+                let shiftVec = new Vector(destX - pawn.pos.x, destY - pawn.pos.y);
+                // get the potential enemy position
+                let enemyVec = shiftVec.divS(2).toObject();
+
+                // check if enemy pos is occupied and if pawn is enemy
+                if (this.pawns.find(p => p.pos.x === enemyVec.x && p.pos.y === enemyVec.y && p.side != pawn.side)) {
+                    // kill the enemy pawn
+                    this.pawns = this.pawns.map(e => {
+                        if (e.pos.x === enemyVec.x && e.pos.y === enemyVec.y) {
+                            return { ...e, isDead: true, pos: { x: -1, y: -1 } }
+                        }
+                    });
+                    // move selected pawn
+                    this.pawns = this.pawns.map(pawn => pawn.pos.x === destX && pawn.pos.y === destY ? { ...pawn, pos: { x: destX, y: destY } } : pawn);
+                }
+            }
+        }
+    }
+}
+
+let game = new GameState("b", "w")
+// validateMove(123, 1, 5)
+// console.log(game.pawns)
+
