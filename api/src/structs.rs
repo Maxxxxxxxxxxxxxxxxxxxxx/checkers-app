d54a8e422fb314298ae620c11a46ae08d095d729
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2022-12-25 18:37:32 +0100

    implement TryFrom trait for Pawn and Game

--- api/src/structs.rs
+++ api/src/structs.rs
@@ -1,3 +1,4 @@
+use neo4rs::Node;
 use serde::{Deserialize, Serialize};
 
 // PAYLOADS --------------------------------
@@ -11,7 +12,19 @@ pub struct Game {
     pub pawns: Vec<Pawn>,
 }
 
-#[derive(Deserialize, Serialize, Clone)]
+impl Game {
+    pub fn from_dbo(dbo: GameDBO, moves: Vec<Move>, pawns: Vec<Pawn>) -> Self {
+        Self {
+            current_color: dbo.current_color,
+            turn: dbo.turn,
+            id: dbo.id,
+            moves,
+            pawns,
+        }
+    }
+}
+
+#[derive(Deserialize, Serialize, Clone, Debug)]
 pub struct Pawn {
     pub is_queen: bool,
     pub is_dead: bool,
@@ -19,6 +32,47 @@ pub struct Pawn {
     pub index: i32,
     pub pos_x: i32,
     pub pos_y: i32,
+}
+
+impl TryFrom<Node> for Pawn {
+    type Error = ();
+    fn try_from(row: Node) -> Result<Self, Self::Error> {
+        let is_queen: Option<bool> = row.get("is_queen");
+        let is_dead: Option<bool> = row.get("is_dead");
+        let index: Option<i64> = row.get("index");
+        let side: Option<String> = row.get("side");
+        let pos_x: Option<i64> = row.get("pos_x");
+        let pos_y: Option<i64> = row.get("pos_y");
+
+        // dbg!(&is_queen);
+        // dbg!(&is_dead);
+        // dbg!(&index);
+        // dbg!(&side);
+
+        match (is_queen, is_dead, index, side, pos_x, pos_y) {
+            (
+                Some(is_queen), 
+                Some(is_dead), 
+                Some(index), 
+                Some(side), 
+                Some(pos_x), 
+                Some(pos_y)
+            ) => {
+                let x32 = pos_x as i32;
+                let y32 = pos_y as i32;
+                let index32 = index as i32;
+                Ok(Pawn {
+                    is_queen,
+                    is_dead,
+                    index: index32,
+                    side,
+                    pos_x: x32,
+                    pos_y: y32,
+                })
+            }
+            _ => Err(()),
+        }
+    }
 }
 
 #[derive(Deserialize, Serialize)]
@@ -53,18 +107,33 @@ pub struct MoveRequest {
 // DB structs
 
 #[derive(Deserialize, Serialize)]
-pub struct GameDB {
+pub struct GameDBO {
     pub current_color: String,
     pub turn: i32,
     pub id: String,
 }
 
-#[derive(Deserialize, Serialize)]
-pub struct PawnDB {
-    pub is_queen: bool,
-    pub is_dead: bool,
-    pub side: String,
-    pub index: i32,
-    pub pos_x: i32,
-    pub pos_y: i32,
+impl TryFrom<Node> for GameDBO {
+    type Error = ();
+    fn try_from(node: Node) -> Result<Self, Self::Error> {
+        let current_color: Option<String> = node.get("current_color");
+        let turn: Option<i64> = node.get("turn");
+        let id: Option<String> = node.get("id");
+
+        match (current_color, turn, id) {
+            (
+                Some(current_color),
+                Some(turn),
+                Some(id),
+            ) => {
+                let turn32 = turn as i32;
+                Ok(GameDBO {
+                    current_color,
+                    turn: turn32,
+                    id
+                })
+            },
+            _ => Err(())
+        }
+    }
 }
