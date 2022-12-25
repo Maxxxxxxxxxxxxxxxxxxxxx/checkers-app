8f14b778b2c7e3d0c87e9b1f9dacbddfd724a810
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2022-12-25 18:35:52 +0100

    add basic get/post db methods

---
+++ api/src/db.rs
@@ -0,0 +1,109 @@
+#![allow(unused)]
+use crate::game::create_pawns;
+use crate::structs::*;
+use neo4rs::*;
+use uuid::Uuid;
+
+const URI: &str = "127.0.0.1:7687";
+const USERNAME: &str = "neo4j";
+const PASSWORD: &str = "neo";
+
+pub async fn connect() -> Result<Graph> {
+    Graph::new(&URI, USERNAME, PASSWORD).await
+}
+
+pub async fn get_game(game_id: &str) -> Result<Game> {
+    let graph = connect().await?;
+
+    let mut game_stream = graph.execute(
+        query("MATCH (game:Game {id: $game_id}) RETURN game")
+        .param("game_id", game_id.clone())
+    ).await?;
+
+    let game_node: Node = game_stream.next().await?.unwrap().get("game").unwrap();
+    let game_dbo: GameDBO = game_node.try_into().unwrap();
+
+    let mut pawns_stream = graph.execute(
+        query("MATCH (:Game {id: $game_id})<-[:PAWN_OF]-(p:Pawn) RETURN p")
+        .param("game_id", game_id.clone())
+    ).await?;
+
+    let mut pawns = Vec::<Pawn>::new();
+    while let Ok(Some(row)) = pawns_stream.next().await {
+        let node: Node = row.get("p").unwrap();
+        let pawn: Pawn = node.try_into().unwrap();
+        // dbg!(pawn);
+        pawns.push(pawn);
+    }
+
+    let game = Game::from_dbo(
+        game_dbo,
+        Vec::<Move>::new(),
+        pawns
+    );
+
+    Ok(game)
+}
+
+pub async fn create_game(pos_white: &str, pos_black: &str) -> Result<()> {
+    match connect().await {
+        Ok(graph) => {
+            let game_id = Uuid::new_v4().to_string();
+            let mut pawns = [
+                create_pawns("w", pos_white), 
+                create_pawns("b", pos_black)
+            ].concat();
+
+            let txn = graph.start_txn().await.unwrap();
+
+            let game_query = query(
+                "
+                CREATE (:Game {
+                    id: $game_id,
+                    current_color: $color,
+                    turn: 1
+                });
+            ",
+            )
+            .param("game_id", game_id.clone())
+            .param("color", "w");
+
+            let mut queries = pawns.into_iter().map(|pawn| {
+                query(
+                    "
+                MATCH (game:Game {id: $game_id})
+                CREATE (p:Pawn {
+                    is_queen: false, 
+                    is_dead: false, 
+                    side: $side,
+                    index: $index,
+                    pos_x: $pos_x,
+                    pos_y: $pos_y
+                })-[:PAWN_OF]->(game)",
+                )
+                .param("game_id", game_id.clone())
+                .param("side", pawn.side.clone())
+                .param("index", pawn.index as i64)
+                .param("pos_x", pawn.pos_x as i64)
+                .param("pos_y", pawn.pos_y as i64)
+            }).collect::<Vec<Query>>();
+
+            queries.insert(0, game_query);
+
+            txn.run_queries(queries).await.unwrap();
+            txn.commit().await
+        }
+        Err(err) => Err(err),
+    }
+}
+
+pub async fn pawn_move(m: Move) {
+
+}
+
+pub async fn exec_query(query_string: &str) -> Result<()> {
+    match connect().await {
+        Ok(graph) => graph.run(query(query_string)).await,
+        Err(err) => Err(err),
+    }
+}
