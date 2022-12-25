fb88cf4843d9ba3a9fa8e2546984b2e3cdd865cd
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2022-12-25 23:22:34 +0100

    rebuilt core components to suit API

--- frontend/src/providers/GameContextProvider.jsx
+++ frontend/src/providers/GameContextProvider.jsx
@@ -7,41 +7,18 @@ export const useGameContext = () => useContext(GameContext);
 
 export default function GameContextProvider({ children }) {
   const [params] = useSearchParams();
+  const [gamestate, setGamestate] = useState();
 
   const gameId = params.get("gameId");
 
-  const [focusedPawn, setFocusedPawn] = useState();
-  const [destinationField, setDestinationField] = useState([]);
-  const [gameState, setGameState] = useState();
-
   useEffect(() => {
-    console.log(gameState);
-    axios
-      .get(`http://127.0.0.1:8000/games/${gameId}`)
-      .then((r) => {
-        setGameState(r.data);
-      })
-      .catch((err) => undefined);
+    axios.get(`http://localhost:8080/games/${gameId}`)
+      .then(res => {setGamestate(res.data); console.log(res.data)})
   }, []);
-
-  useEffect(() => {
-    if (focusedPawn && destinationField.length === 2) {
-      axios.put(
-        `http://127.0.0.1:8000/games/game/${gameId}?move&${focusedPawn[2]}&<index>&<x>&<y>`
-      );
-    }
-  }, [destinationField]);
-
-  const clearSelection = () => {
-    setFocusedPawn([]);
-    setDestinationField([]);
-  };
-  const setFocus = (pawn) => setFocusedPawn(pawn);
-  const setDest = (x, y) => setDestinationField([x, y]);
 
   return (
     <GameContext.Provider
-      value={{ focusedPawn, clearSelection, setFocus, setDest, gameState }}
+      value={{ gamestate }}
     >
       {children}
     </GameContext.Provider>
