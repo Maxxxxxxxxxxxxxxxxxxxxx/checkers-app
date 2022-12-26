589c2c662f4dc9d6b7e0e84a46e5d7c6d796a681
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2022-12-26 16:50:23 +0100

    add basic pawn move feature

--- frontend/src/providers/GameContextProvider.jsx
+++ frontend/src/providers/GameContextProvider.jsx
@@ -1,5 +1,7 @@
-import { useState, createContext, useContext, useEffect } from "react";
+import { useState, useReducer, createContext, useContext, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
+import GamestateReducer from "./GamestateReducer";
+import * as Actions from './GamestateActions';
 import axios from "axios";
 
 const GameContext = createContext();
@@ -7,18 +9,30 @@ export const useGameContext = () => useContext(GameContext);
 
 export default function GameContextProvider({ children }) {
   const [params] = useSearchParams();
-  const [gamestate, setGamestate] = useState();
+  const [gamestate, dispatch] = useReducer(GamestateReducer, {pawns: []});
+  const [moveParams, setMoveParams] = useState({});
 
   const gameId = params.get("gameId");
 
+  const focusPawn = (pawn) => setMoveParams({...moveParams, pawn});
+  const focusDest = (x, y) => setMoveParams({...moveParams, dest: [x, y]})
+  const clearFocus = () => setMoveParams({});
+
+  useEffect(() => {
+    if (moveParams.pawn && moveParams.dest) {
+      dispatch(Actions.move(moveParams.pawn, moveParams.dest[0], moveParams.dest[1]))
+      clearFocus();
+    }
+  })
+
   useEffect(() => {
     axios.get(`http://localhost:8080/games/${gameId}`)
-      .then(res => {setGamestate(res.data); console.log(res.data)})
+      .then(res => { dispatch(Actions.set(res.data)); console.log(res.data) })
   }, []);
 
   return (
     <GameContext.Provider
-      value={{ gamestate }}
+      value={{ gamestate, dispatch, focusPawn, focusDest }}
     >
       {children}
     </GameContext.Provider>
--- frontend/src/views/Game/Field.jsx
+++ frontend/src/views/Game/Field.jsx
@@ -1,22 +1,25 @@
 import { useGameContext } from "@/providers/GameContextProvider";
+import * as Actions from '@/providers/GamestateActions';
 
 export default function Field({ x, y }) {
-  let { gamestate } = useGameContext();
+  let { gamestate, focusPawn, focusDest } = useGameContext();
 
-  let pawn = gamestate 
-    ? gamestate.pawns.find(pawn => pawn.pos_x === x && pawn.pos_y === y)
+  let pawn = gamestate
+    ? gamestate.pawns.find((pawn) => pawn.pos_x === x && pawn.pos_y === y)
     : undefined;
 
   return pawn ? (
-    <div
-      className={`game__field game__field--occupied`}
-    >
+    <div className={`game__field game__field--occupied`}>
       <img
-        src={pawn.side === 'b' ? '/pawn_black.svg' : '/pawn_white.svg'}
+        src={pawn.side === "b" ? "/pawn_black.svg" : "/pawn_white.svg"}
         alt=""
         className="sprite"
         onClick={() => {
           console.log(pawn);
+          focusPawn(pawn);
+          // dispatch({type: 'MOVE', pawn: pawn, x: 1, y: 4 })
+          // dispatch(Actions.move(pawn, 1, 4))
+          // setPawn({ x: pawn.pos_x, y: pawn.pos_y })
           // setFocus(color, index);
         }}
       />
@@ -26,10 +29,10 @@ export default function Field({ x, y }) {
       className={`game__field game__field--empty`}
       onClick={() => {
         console.log(x, y);
-        // setDest(x, y);
-        // clearSelection();
+        focusDest(x,y);
+        // setDestination({ x, y });
+        // clearFocus();
       }}
-    >
-    </div>
+    ></div>
   );
 }
