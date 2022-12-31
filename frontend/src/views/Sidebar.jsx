32c156e5d3b856963289d32df6b299c507d9f3dd
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2022-12-31 16:40:48 +0100

    added Sidebar

---
+++ frontend/src/views/Sidebar.jsx
@@ -0,0 +1,10 @@
+import { Box, AppBar, Toolbar } from "@mui/material/index";
+import "@/styles/SideBar/Sbar.css";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+
+    </div>
+  )
+}
