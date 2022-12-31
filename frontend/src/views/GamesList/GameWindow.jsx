6ac51886c0c2c6b5b14ed0e5885bb9b2b03906ad
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2022-12-31 16:39:38 +0100

    add links to GameWindow

--- frontend/src/views/GamesList/GameWindow.jsx
+++ frontend/src/views/GamesList/GameWindow.jsx
@@ -1,4 +1,5 @@
 import { Box, ButtonGroup, Button, Typography } from '@mui/material/index';
+import { Link } from "react-router-dom";
 
 export default function GameWindow({ gamestate }) {
   let linkBlack = `/game?id=${gamestate.id}&player=black`;
@@ -8,12 +9,16 @@ export default function GameWindow({ gamestate }) {
     <Box className="element">
       <img src="" alt="" className='layout'/>
       <Typography>
-        {/* { gamestate.id } */}
-        game name
+        { gamestate.name }
+        {/* game name */}
       </Typography>
       <ButtonGroup variant="contained" aria-label="outlined primary button group">
-        <Button className="button--white">Play white</Button>
-        <Button className="button--black">Play black</Button>
+        <Link to={linkWhite}>
+          <Button className="button--white">Play white</Button> 
+        </Link>
+        <Link to={linkBlack}>
+          <Button className="button--black">Play black</Button>
+        </Link>
       </ButtonGroup>
     </Box>
   )
