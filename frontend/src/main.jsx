e74e1fc7a58fbcbe1d85603c1d1fb5a51a1ca83b
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2023-01-01 23:14:27 +0100

    added mui theme override

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,20 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: 'rgba(255, 255, 255, 0.87)',
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+    <ThemeProvider theme={theme}>
       <App />
+    </ThemeProvider>
   </BrowserRouter>
 )
