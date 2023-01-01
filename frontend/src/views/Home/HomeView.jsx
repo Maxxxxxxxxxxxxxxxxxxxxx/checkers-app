f1683fed78a0b67731219e168d756f0f693567da
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2023-01-02 00:33:11 +0100

    add sidebar and chat to home

--- frontend/src/views/Home/HomeView.jsx
+++ frontend/src/views/Home/HomeView.jsx
@@ -1,32 +1,39 @@
 import "@/styles/Home/Home.css";
 import { Link } from 'react-router-dom';
 import ViewListRoundedIcon from '@mui/icons-material/ViewListRounded';
+import { Fragment } from "react";
+import Sidebar from "../Sidebar";
+import ChatTab from "../ChatTab/ChatTab";
 
 export default function HomeView() {
   return (
-    <div className="view">
-      <div className="home-window">
-        <div className="home-window__header"> 
-          Checkers 
+    <Fragment>
+      <Sidebar />
+      <div className="view">
+        <div className="home-window">
+          <div className="home-window__header"> 
+            Checkers 
+          </div>
+          <div className="home-window__buttons">
+            <Link to="/list">
+              <button className="big-button">
+                <div className="big-button__content">
+                  <ViewListRoundedIcon></ViewListRoundedIcon>
+                  Games
+                </div>
+              </button>
+            </Link>
+            <Link to="/newgame">
+              <button className="big-button">
+                <div className="big-button__content">
+                  New game
+                </div>
+              </button>
+            </Link>
+          </div>
         </div>
-        <div className="home-window__buttons">
-          <Link to="/list">
-            <button className="big-button">
-              <div className="big-button__content">
-                <ViewListRoundedIcon></ViewListRoundedIcon>
-                Games
-              </div>
-            </button>
-          </Link>
-          <Link to="/newgame">
-            <button className="big-button">
-              <div className="big-button__content">
-                New game
-              </div>
-            </button>
-          </Link>
-        </div>
+      <ChatTab />
       </div>
-    </div>
+    </Fragment>
   )
 }
