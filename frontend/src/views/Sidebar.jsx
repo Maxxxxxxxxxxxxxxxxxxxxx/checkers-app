98c52d35bf71b338d2dd0d150653289c664d8fa4
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2023-01-02 02:26:50 +0100

    added links and logo to sidebar

--- frontend/src/views/Sidebar.jsx
+++ frontend/src/views/Sidebar.jsx
@@ -1,10 +1,30 @@
-import { Box, AppBar, Toolbar } from "@mui/material/index";
 import "@/styles/SideBar/Sbar.css";
+import { Typography } from "@mui/material/index";
+import { Link } from "react-router-dom";
+import { Menu } from "@mui/icons-material/index";
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import HomeIcon from '@mui/icons-material/Home';
 
 export default function Sidebar() {
   return (
     <div className="sidebar">
-
+      <Typography className="logo" variant="p" component="div">
+        Checkers.com
+      </Typography>
+      <div className="sidebar__links">
+        <Link to="/home" className="sidebar__link">
+          <HomeIcon className="sidebar__link-icon"/>
+          Home
+        </Link>
+        <Link to="/list" className="sidebar__link">
+          <Menu className="sidebar__link-icon"/> 
+          Games
+        </Link>
+        <Link to="/newgame" className="sidebar__link">
+          <AddBoxIcon className="sidebar__link-icon" />
+          New
+        </Link>
+      </div>
     </div>
   )
 }
