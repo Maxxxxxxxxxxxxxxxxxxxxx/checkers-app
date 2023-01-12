ea46d5299bd3a4cee8dac0c1ff091a551f8ed281
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2023-01-12 18:54:14 +0100

    add hook + responsive sidebar for <1000px

--- frontend/src/providers/Sidebar/SidebarProvider.jsx
+++ frontend/src/providers/Sidebar/SidebarProvider.jsx
@@ -1,3 +1,4 @@
+import useWindowDimensions from "@/hooks/useWindowDimensions";
 import { useEffect } from "react";
 import { useContext, useState } from "react";
 import { createContext } from "react";
@@ -6,9 +7,14 @@ const SidebarContext = createContext();
 export const useSidebarContext = () => useContext(SidebarContext);
 
 export default function SidebarContextProvider({ children }) {
+    let { width } = useWindowDimensions();
     let [folded, setFold] = useState(false);
     let [sidebarMargin, setMargin] = useState("170px");
 
+    useEffect(() => {
+        width < 1000 ? setFold(true) : undefined
+    }, [width])
+    
     useEffect(() => {
         folded ? setMargin("3rem") : setMargin("170px")
     }, [folded])
