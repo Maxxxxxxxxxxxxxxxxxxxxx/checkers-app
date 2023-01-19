acefd7b5202595e1d9bdc1f449fbec9d11ebe529
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2023-01-19 10:13:17 +0100

    rename "forum" to "comments"

--- api/src/schema/mod.rs
+++ api/src/schema/mod.rs
@@ -4,7 +4,7 @@ use std::time::SystemTime;
 use struct_field_names_as_array::FieldNamesAsArray;
 
 pub mod auth;
-pub mod forum;
+pub mod comment;
 pub mod game;
 pub mod gamemove;
 pub mod pawn;
@@ -12,7 +12,7 @@ pub mod post_bodies;
 pub mod user;
 
 pub use auth::*;
-pub use forum::*;
+pub use comment::*;
 pub use game::*;
 pub use gamemove::*;
 pub use pawn::*;
