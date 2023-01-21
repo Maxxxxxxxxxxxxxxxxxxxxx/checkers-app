9245d0cbb03b3e88509896975ce5b127a34e0dec
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2023-01-21 20:51:08 +0100

    update game preview display

--- frontend/src/views/GamesList/GameWindow.jsx
+++ frontend/src/views/GamesList/GameWindow.jsx
@@ -1,35 +1,59 @@
-import { Box, Button, Typography } from '@mui/material/index';
-import BoardPreview from '@/views/Preview/BoardPreview';
-import { useNavigate } from 'react-router-dom';
+import { Box, Button, Typography } from "@mui/material/index";
+import BoardPreview from "@/views/Preview/BoardPreview";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function GameWindow({ gamestate }) {
   let linkBlack = `/game?id=${gamestate.id}&player=b`;
   let linkWhite = `/game?id=${gamestate.id}&player=w`;
 
+  let [comments, setComments] = useState(0);
+
   let navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/games/game/${gamestate.id}/comments`)
+      .then((res) => setComments(res.data));
+  }, []);
 
   return (
     <Box className="element">
       <BoardPreview gamestate={gamestate} />
       <div className="element__info">
-        <Typography variant="h4" sx={{fontWeight: 1000, fontSize: "1.5rem"}} className="element__name">
-          { gamestate.name }
-        </Typography>
-        <div className="element__labels">
-          <div className="element__buttons">
-            <Button sx={{background: "rgb(127,166,80)"}} onClick={() => navigate(linkWhite)}>
-              <span className="pawn-white-icon">
-
-              </span>
-            </Button>
-            <Button sx={{background: "rgb(127,166,80)"}} onClick={() => navigate(linkBlack)}>
-              <span className="pawn-black-icon">
-              </span>
-            </Button>
+          <Typography
+            variant="h4"
+            sx={{ fontWeight: 1000, fontSize: "2.5rem" }}
+            className="element__name"
+          >
+            {gamestate.name}
+          </Typography>
+          <div className="element__labels">
+            <div className="element__buttons">
+              <button
+                className="big-button element__btn"
+                onClick={() => navigate(linkBlack)}
+              >
+                Play as black
+              </button>
+              <button
+                className="big-button element__btn"
+                onClick={() => navigate(linkWhite)}
+              >
+                Play as white
+              </button>
+            </div>
+            <Typography
+              className="element__comments-link"
+              variant="p"
+              sx={{ color: "grey", fontWeight: 1000, alignSelf: "flex-end" }}
+              onClick={() => navigate(`/comments/${gamestate.id}`)}
+            >
+              Comments ({comments.length})
+            </Typography>
           </div>
-          <Typography variant="p" sx={{color: "grey", fontWeight: 1000}}>Comments (0)</Typography>
-        </div>
       </div>
     </Box>
-  )
+  );
 }
