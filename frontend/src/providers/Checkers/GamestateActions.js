0544ea430eae31e0cf4da8034ce6eea7b39d9af6
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2023-01-21 20:49:51 +0100

    update actions

--- frontend/src/providers/Checkers/GamestateActions.js
+++ frontend/src/providers/Checkers/GamestateActions.js
@@ -1,12 +1,18 @@
-export const move = (pawn, x, y, playerColor) => ({
+export const move = (pawn, x, y, playerColor, publish) => ({
   type: "MOVE",
   pawn: pawn,
   x: x,
   y: y,
-  playerColor
+  playerColor,
+  publish
 });
 
 export const set = (newState) => ({
   type: 'SET',
   newState,
+});
+
+export const end = (winner) => ({
+  type: 'SET',
+  winner,
 });
