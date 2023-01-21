2fdd92894777e47774d52b7b0ff631e0245e2ca9
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2023-01-21 16:02:36 +0100

    added beer endpoints

--- api/src/crud/comments/controllers.rs
+++ api/src/crud/comments/controllers.rs
@@ -3,9 +3,9 @@ use crate::schema::*;
 use crate::utils::*;
 use actix_web::HttpRequest;
 use actix_web::web::Json;
-use actix_web::{delete, get, post, put, web, HttpResponse};
+use actix_web::{delete, get, post, put, HttpResponse};
 
-#[get("/comments/{game_id}")]
+#[get("games/game/{game_id}/comments")]
 pub async fn of_game(req: HttpRequest) -> HttpResponse {
     let gameid = req.match_info().query("game_id");
     match db::comment::of_game(gameid.to_string()).await {
@@ -14,22 +14,35 @@ pub async fn of_game(req: HttpRequest) -> HttpResponse {
     }
 }
 
-#[get("/comments/{game_id}/count")]
+#[get("games/game/{game_id}/comments/count")]
 pub async fn count(req: HttpRequest) -> HttpResponse {
-    unimplemented!();
+    let id = req.match_info().query("game_id").to_string();
+    match db::comment::count_for_game(id).await {
+        Ok(int) => ResponseType::Ok(int).get_response(),
+        Err(_) => ResponseType::NotFound("Failed to fetch comments!").get_response()
+    }
 }
 
 #[get("/comments")]
-pub async fn all(req: HttpRequest) -> HttpResponse {
-    unimplemented!();
+pub async fn count_all() -> HttpResponse {
+    match db::comment::count_all().await {
+        Ok(int) => ResponseType::Ok(int).get_response(),
+        Err(_) => ResponseType::NotFound("Failed to fetch comments!").get_response()
+    }
 }
 
-#[put("/comments/{game_id}/{comment_id}")]
-pub async fn edit(req: HttpRequest) -> HttpResponse {
-    unimplemented!();
+#[put("/comments/{comment_id}")]
+pub async fn edit(req: HttpRequest, payload: Json<AddComment>) -> HttpResponse {
+    let cid = req.match_info().query("comment_id");
+    let addcomment = payload.into_inner();
+
+    match db::comment::edit(cid.to_string(), addcomment).await {
+        Ok(c) => ResponseType::Ok(c).get_response(),
+        Err(_) => ResponseType::NotFound("Failed to edit comment!").get_response()
+    }
 }
 
-#[post("/comments/{game_id}")]
+#[post("games/game/{game_id}/comments")]
 pub async fn add(req: HttpRequest, payload: Json<AddComment>) -> HttpResponse {
     let gameid = req.match_info().query("game_id");
     let comment = Comment::from(payload.into_inner());
@@ -45,11 +58,12 @@ pub async fn add(req: HttpRequest, payload: Json<AddComment>) -> HttpResponse {
     }
 }
 
-#[post("/comments/comment/{comment_id}")]
-pub async fn give_beer(req: HttpRequest, payload: Json<AddBeer>) -> HttpResponse {
+#[post("/comments/comment/{comment_id}/beer/{author}")]
+pub async fn give_beer(req: HttpRequest) -> HttpResponse {
     let id = req.match_info().query("comment_id");
+    let author = req.match_info().query("author");
 
-    match db::comment::give_beer(id.to_string(), payload.author.to_string()).await {
+    match db::comment::give_beer(id.to_string(), author.to_string()).await {
         Ok(p) => { 
             ResponseType::Ok(p).get_response() 
         },
@@ -57,5 +71,31 @@ pub async fn give_beer(req: HttpRequest, payload: Json<AddBeer>) -> HttpResponse
             log::error!("Error adding beer!");
             ResponseType::BadRequest("Request must include author username!").get_response()
         }
+    }
+}
+
+#[delete("/comments/comment/{comment_id}/beer/{author}")]
+pub async fn remove_beer(req: HttpRequest) -> HttpResponse {
+    let id = req.match_info().query("comment_id");
+    let author = req.match_info().query("author");
+
+    match db::comment::remove_beer(id.to_string(), author.to_string()).await {
+        Ok(p) => { 
+            ResponseType::Ok(p).get_response() 
+        },
+        Err(_) => {
+            log::error!("Error removing beer!");
+            ResponseType::NotFound("Beer not found!").get_response()
+        }
+    }
+}
+
+#[delete("/comments/comment/{comment_id}")]
+pub async fn delete(req: HttpRequest) -> HttpResponse {
+    let id = req.match_info().query("comment_id");
+
+    match db::comment::delete(id.to_string()).await {
+        Ok(()) => ResponseType::Ok(()).get_response(),
+        Err(_) => ResponseType::NotFound("Failed to delete / not found!").get_response()
     }
 }
