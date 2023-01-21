ed23a2c587dfefa90a2ca31a2a50cdb893aafbea
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2023-01-21 20:47:12 +0100

    updated GameInfo

--- frontend/src/views/Game/GameInfo.jsx
+++ frontend/src/views/Game/GameInfo.jsx
@@ -1,19 +1,24 @@
 import { useGameContext } from "@/providers/Checkers/GameContextProvider";
-import { Typography } from "@mui/material";
+import { Typography, Box } from "@mui/material";
+import { useSearchParams } from "react-router-dom";
 
 export default function GameInfo() {
-    let { gamestate } = useGameContext();
+  let { gamestate } = useGameContext();
+  let [params] = useSearchParams();
 
-    return (
-        <div className="game-info">
-            <Typography variant="h4" sx={{textAlign: "center"}}>
-                Playing as { gamestate.current_color === "w" ? "WHITE" : "BLACK" }
-            </Typography>
-            <div className="game-info__params">
-                <Typography variant="p">
-                    Move: { gamestate.current_color === "w" ? "WHITE" : "BLACK" }
-                </Typography>
-            </div>
-        </div>
-    )
+  let currentColor = params.get("player");
+
+  return (
+    <div className="game-info">
+      <Typography variant="h4" sx={{ textAlign: "center" }}>
+        Playing as {currentColor === "w" ? "WHITE" : "BLACK"}
+      </Typography>
+      <Box sx={{ marginTop: "2rem" }} className="game-info__params">
+        <Typography variant="p">
+          Now moving: {gamestate.current_color === "w" ? "WHITE" : "BLACK"}
+        </Typography>
+        <Typography variant="p">Turn: {gamestate.turn}</Typography>
+      </Box>
+    </div>
+  );
 }
