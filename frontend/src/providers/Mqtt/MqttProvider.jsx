70c832abc6f42989c99c7b925637e36d22ecbf4f
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2023-01-21 20:46:35 +0100

    refactor and fix MqttProvider

--- frontend/src/providers/Mqtt/MqttProvider.jsx
+++ frontend/src/providers/Mqtt/MqttProvider.jsx
@@ -1,55 +1,44 @@
 import { useState, createContext, useContext, useEffect } from "react";
-
-import { useSearchParams } from "react-router-dom";
 import { useAuthUser } from "react-auth-kit";
 import * as mqtt from "mqtt/dist/mqtt";
-import axios from "axios";
 
-const MqttContext = createContext();
-export const useMqtt = () => useContext(MqttContext);
+const CommentsContext = createContext();
+export const useMqtt = () => useContext(CommentsContext);
 
-export default function MqttContextProvider({ children }) {
+export default function MqttProvider({ children }) {
   const auth = useAuthUser();
   const username = auth();
   const [client, setClient] = useState();
+  const [subscribedTopics, setSubscribedTopics] = useState([]);
+  
+  setTimeout(() => console.log(subscribedTopics) , 2000)
 
   useEffect(() => {
+    if(client) client.end();
     const c = mqtt.connect("ws://127.0.0.1", {
       port: 8081,
     });
 
-    c.subscribe("test")
-    c.on("message", (topic, msg) => console.log("MQTT MESSAGE:", msg.toString()))
-
-    // console.log("Connected to broker as", username || "(NO USER)", "connection:", c);
     setClient(c);
   }, [username]);
 
-  const test = () => client.publish("test", `${username} sends a ping`)
+  const subscribe = (topic) => {
+    if (client && !subscribedTopics.includes(topic)) { 
+      client.subscribe(topic) 
+      setSubscribedTopics([...subscribedTopics, topic])
+    }
+  }
 
-  const comments = {
-    connect: (callback) => {
-      client.subscribe("comments");
-      client.on("message", (topic, message) => {
-        if (topic == "comments") callback(message);
-      });
-    },
-    publish: (payload) => username && client.publish("comments", payload),
-  };
-
-  const game = {
-    connect: (callback) => {
-      client.subscribe("game");
-      client.on("message", (topic, message) => {
-        if (topic == "game") callback(message);
-      });
-    },
-    publish: (payload) => username && client.publish("comments", payload),
-  };
+  const unsubscribe = (topic) => {
+    if(client) {
+      client.unsubscribe(topic)
+      setSubscribedTopics(subscribedTopics.filter(t => t != topic))
+    }
+  }
 
   return (
-    <MqttContext.Provider value={{ client, comments, game, test }}>
+    <CommentsContext.Provider value={{ client, subscribe, unsubscribe }}>
       {children}
-    </MqttContext.Provider>
+    </CommentsContext.Provider>
   );
 }
