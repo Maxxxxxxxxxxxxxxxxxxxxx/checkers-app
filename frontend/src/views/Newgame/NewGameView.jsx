4a23a4b6c1ab8d50cd75f2421f314fa8bda0d202
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2023-01-24 14:11:51 +0100

    refactor of newGame

--- frontend/src/views/Newgame/NewGameView.jsx
+++ frontend/src/views/Newgame/NewGameView.jsx
@@ -1,6 +1,6 @@
 import { Toolbar, Typography } from "@mui/material/index";
 
-import { Fragment, useEffect, useState } from "react";
+import { useLayoutEffect, useState } from "react";
 import "@/styles/Newgame/Newgame.css";
 import AddBoxIcon from "@mui/icons-material/AddBox";
 import axios from "axios";
@@ -8,38 +8,45 @@ import BoardPreview from "../Preview/BoardPreview";
 import View from "../View";
 import { useForm } from "react-hook-form";
 import { useAuthUser } from "react-auth-kit";
+import { useNavigate } from "react-router-dom";
 
 export default function NewGameView() {
+  const auth = useAuthUser();
+  const navigate = useNavigate();
   const [previewState, setPreviewState] = useState();
-  const [params, setParams] = useState({
-    white: "top",
-    black: "bottom",
-  });
-  
-  useEffect(() => {
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    axios
+      .post("http://localhost:8080/games/new_game", {
+        author: auth(),
+        mode: data.mode ? data.mode : "easy",
+        white: data.topPos === "w" ? "top" : "bottom",
+        black: data.topPos === "b" ? "top" : "bottom",
+        name: data.name ? data.name : `${auth()}'s game`,
+      })
+      .then((res) =>
+        navigate(
+          `/game?id=${res.data.id}&player=${data.playAs}`
+        )
+      );
+  };
+
+  useLayoutEffect(() => {
     axios
       .put("http://localhost:8080/games/preview", {
-        ...params,
-        name: "dummy",
+        author: auth(),
+        name: `${auth()}'s game`,
+        white: "top",
+        black: "bottom",
         mode: "easy",
       })
       .then((res) => setPreviewState(res.data));
-    }, [params]);
-    
-  const auth = useAuthUser()
-
-  console.log("user", auth());
-
-  const { register, handleSubmit } = useForm({
-    shouldUseNativeValidation: true,
-    // defaultValues: {
-    //   name: `${auth().username}'s game`
-    // }
-  });
-
-  const onSubmit = async (data) => {
-    
-  };
+  }, []);
 
   return (
     <View>
@@ -61,17 +68,32 @@ export default function NewGameView() {
           <form className="newgame__form" onSubmit={handleSubmit(onSubmit)}>
             <input
               type="text"
-              placeholder="Game name"
+              placeholder={`${auth()}'s game`}
               className="newgame__textfield"
-              {...register("name", { required: "Enter game name" })}
+              {...register("gameName", {
+                required: false,
+                max: 20,
+                min: 3,
+                maxLength: 20,
+              })}
             />
-            <div className="newgame__checkbox-group">
-              <input
-                type="checkbox"
-                {...register("mode", { required: "Enter username" })}
-              />{" "}
-              Hard mode
+            <div className="newgame__select-group">
+              <select {...register("topPos")} className="newgame__selector">
+                <option value="w">white on top</option>
+                <option value="b">black on top</option>
+              </select>
+              <select {...register("mode")} className="newgame__selector">
+                <option value="easy">Easy</option>
+                <option value="hardcore">Hardcore</option>
+              </select>
+              <select {...register("playAs")} className="newgame__selector">
+                <option value="w">Play as white</option>
+                <option value="b">Play as black</option>
+              </select>
             </div>
+            <button className="small-button" type="submit">
+              Submit
+            </button>
           </form>
           {previewState ? (
             <BoardPreview gamestate={previewState} />
