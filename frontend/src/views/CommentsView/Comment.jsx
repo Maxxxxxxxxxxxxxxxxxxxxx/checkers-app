6fe48bd556b7b718e216cd1e79cadfa31e76c1e4
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2023-01-24 14:10:51 +0100

    refactor Comment component

--- frontend/src/views/CommentsView/Comment.jsx
+++ frontend/src/views/CommentsView/Comment.jsx
@@ -3,7 +3,7 @@ import { IconButton } from "@mui/material";
 import { useDispatch } from "react-redux";
 import { commentActions as Actions } from "@/providers/Comments/CommentsReducer";
 import { useAuthUser } from "react-auth-kit";
-import { useState, useEffect } from "react";
+import { useState, useLayoutEffect } from "react";
 import { useMqtt } from "@/providers/Mqtt/MqttProvider";
 import axios from "axios";
 
@@ -11,67 +11,137 @@ export default function Comment({ state }) {
   const [isBeerGiven, setBeerGiven] = useState(false);
   let dispatch = useDispatch();
   let auth = useAuthUser();
+
   const { client, subscribe } = useMqtt();
+  const [isEdit, setEdit] = useState(false);
+  const [editState, setEditState] = useState({
+    title: state.title,
+    content: state.content,
+  });
 
-  console.log("comment state", state)
-  console.log(isBeerGiven)
+  const lastEditedMessage = `Last edited ${new Date(
+    state.timestamp * 1000
+  )}`.slice(0, 36);
 
-  useEffect(() => {
-    subscribe("beers")
-    if(state.beers.find(beer => beer.author == auth())) setBeerGiven(true)
-  }, [])
+  // console.log("comment state", state);
+  // console.log(isBeerGiven);
+
+  useLayoutEffect(() => {
+    subscribe("beers");
+    if (state.beers.find((beer) => beer.author == auth())) setBeerGiven(true);
+  }, []);
 
   const giveBeer = () => {
     axios
       .post(`http://localhost:8080/comments/comment/${state.id}/beer/${auth()}`)
-      .then(res => {
-        console.log("New beer: ", res.data)
-        dispatch(Actions.GiveBeer({ beer: res.data, id: state.id }))
-        setBeerGiven(true)
-      })
-  }
+      .then((res) => {
+        console.log("New beer: ", res.data);
+        dispatch(Actions.GiveBeer({ beer: res.data, id: state.id }));
+        setBeerGiven(true);
+      });
+  };
 
   const removeBeer = () => {
     axios
-      .delete(`http://localhost:8080/comments/comment/${state.id}/beer/${auth()}`)
-      .then(res => {
-        console.log("Deleted beer", res.data)
-        dispatch(Actions.RemoveBeer({ author: auth(), id: state.id }))
-        setBeerGiven(false)
-      })
-  }
+      .delete(
+        `http://localhost:8080/comments/comment/${state.id}/beer/${auth()}`
+      )
+      .then((res) => {
+        console.log("Deleted beer", res.data);
+        dispatch(Actions.RemoveBeer({ author: auth(), id: state.id }));
+        setBeerGiven(false);
+      });
+  };
 
-  const handleBeerClick = () => isBeerGiven ? removeBeer() : giveBeer();
+  const handleBeerClick = () => (isBeerGiven ? removeBeer() : giveBeer());
 
-  const handleDelete = () => axios
-    .delete(`http://localhost:8080/comments/comment/${state.id}`)
-    .then(res => {
-      console.log("Deleted comment")
-      // dispatch(Actions.Delete(state.id))
-      client.publish("comments/delete", state.id)
+  const submitEdit = () => {
+    axios.put(`http://localhost:8080/comments/comment/${state.id}`, {
+      author: auth(),
+      title: editState.title,
+      content: editState.content
     })
+    .then(res => {
+      client.publish("comments/edit", JSON.stringify({
+        ...state,
+        title: res.data.title,
+        content: res.data.content
+      }))
+      setEdit(false)
+    })
+    .catch(err => {
+      alert("Connection error! Can't edit comment!")
+      setEdit(false)
+    })
+    
+  };
+
+  const handleDelete = () =>
+    axios
+      .delete(`http://localhost:8080/comments/comment/${state.id}`)
+      .then((res) => {
+        console.log("Deleted comment");
+        // dispatch(Actions.Delete(state.id))
+        client.publish("comments/delete", state.id);
+      });
 
   return (
     <div className="comment">
       <div className="comment__body">
         <div className="comment__head">
-          <div className="comment__title"> {state.title} </div>
+          {isEdit ? (
+            <input
+              type="text"
+              className="comment__edit-title"
+              defaultValue={state.title}
+              onChange={(event) =>
+                setEditState({ ...editState, title: event.target.value })
+              }
+            />
+          ) : (
+            <div className="comment__title"> {state.title} </div>
+          )}
           <div className="comment__author"> {state.author} </div>
-          <div className="comment__date">
-            {" "}
-            {`Last edited ${new Date(state.timestamp * 1000)}`.slice(0, 36)}{" "}
-          </div>
+          <div className="comment__date">{lastEditedMessage}</div>
         </div>
-        <p className="comment__content">{state.content}</p>
+        {isEdit ? (
+          <textarea
+            defaultValue={state.content}
+            className="comment__edit-content"
+            onChange={(event) =>
+              setEditState({ ...editState, content: event.target.value })
+            }
+          ></textarea>
+        ) : (
+          <p className="comment__content">{state.content}</p>
+        )}
       </div>
-      <div className="comment__rightside">
-        <div className={ isBeerGiven ? "beer beer--given" : "beer" }>
+      <div
+        className={`comment__rightside${
+          state.author === auth() ? " comment__rightside--buttons" : ""
+        }`}
+      >
+        <div className={isBeerGiven ? "beer beer--given" : "beer"}>
           <IconButton aria-label="beer" onClick={() => handleBeerClick()}>
             <SportsBarIcon></SportsBarIcon>
           </IconButton>
           <span className="beer__amount">{state.beers.length}</span>
         </div>
-        { state.author === auth() && <button className="small-button" onClick={() => handleDelete()}>Delete</button>}
+        <div className="comment__buttons">
+          {state.author === auth() && (
+            <button className="small-button" onClick={() => handleDelete()}>
+              Delete
+            </button>
+          )}
+          {state.author === auth() && (
+            <button
+              className="small-button"
+              onClick={() => (isEdit ? submitEdit() : setEdit(true))}
+            >
+              {isEdit ? "Submit edit" : "Edit"}
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
