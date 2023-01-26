b24f945c60f3e0040e4a3270a7b8ac9007b90ff2
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2023-01-26 11:42:32 +0100

    add profileview

--- frontend/src/views/ProfileView/ProfileView.jsx
+++ frontend/src/views/ProfileView/ProfileView.jsx
@@ -1,6 +1,27 @@
 import View from "../View";
+import Form from "../Users/Form";
+import { useSignOut } from "react-auth-kit";
+import { useNavigate } from "react-router-dom";
+import { Toolbar, IconButton, Menu, Typography } from "@mui/material/";
+import { useAuthUser } from "react-auth-kit";
+import axios from "axios";
 
 export default function ProfileView() {
+  const nav = useNavigate();
+  const signOut = useSignOut();
+  const auth = useAuthUser();
+
+  const act = (data) => {
+    if(auth() === "admin") return alert("Can't modify admin credentials!")
+
+    axios.put(`http://localhost:8080/user/users/edit/${auth()}`, data)
+      .then(res => {
+        signOut();
+        nav("/login");
+      })
+      .catch(err => alert("Failed to modify user credentials!"))
+  }
+
   return (
     <View>
       <div className="profile-view">
@@ -15,12 +36,12 @@ export default function ProfileView() {
               color="inherit"
               component="div"
             >
-              {gamestate.name}
+              {`Editing ${auth()}`}
             </Typography>
           </span>
         </Toolbar>
         <section className="profile">
-          
+          <Form action={(data) => act(data)}></Form>
         </section>
       </div>
     </View>
